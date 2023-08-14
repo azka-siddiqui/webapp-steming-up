d681c92d340cc4288f5b66947207e0c3e5a19530
Azka Siddiqui <129907688+azka-siddiqui@users.noreply.github.com>
2023-08-14 11:48:18 -0400

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,65 @@
+import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+import "@fontsource/montserrat"
+import "@fontsource/oswald"
+import { Global, css } from '@emotion/react'
+import { useColorMode } from '@chakra-ui/react'
+import { useEffect } from 'react'
+
+
+const theme = extendTheme({
+  fonts: {
+    heading: 'Oswald',
+    body: 'Montserrat',
+  },
+})
+
+function ForceLightMode({ children }) {
+  // force light mode b/c of ChakraUI bug
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  useEffect(() => {
+    if (colorMode === "light") return;
+    toggleColorMode();
+  }, [colorMode, toggleColorMode]);
+
+  return children;
+}
+
+const GlobalStyle = ({ children }) => {
+  let { colorMode } = useColorMode()
+  return (
+    <>
+      <Global
+        styles={css`
+          html {
+            min-width: 356px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+            background: #fffff;
+          }
+        `}
+      />
+      {children}
+    </>
+  )
+}
+
+
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <ForceLightMode>
+        <Component {...pageProps} />
+      </ForceLightMode>
+    </ChakraProvider>
+  )
+}
+
+export default MyAp
