2298ce2ed7891c67df25060377ceda859f94af81
AlizaFaraz <110481621+AlizaFaraz@users.noreply.github.com>
2023-08-18 06:08:38 -0400

    Update form.jsx

--- components/form/form.jsx
+++ components/form/form.jsx
@@ -1,29 +1,97 @@
-// FormComponent.jsx
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import './form.css'; // Import your CSS file for styling
 
-const FormComponent = () => {
-  const [formData, setFormData] = useState({
-    // Initialize form fields here
-  });
+const YourFormComponent = () => {
+  const [submissionType, setSubmissionType] = useState('text');
+  const [file, setFile] = useState(null);
+  const [idea, setIdea] = useState('');
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
+  const handleSubmissionTypeChange = (event) => {
+    setSubmissionType(event.target.value);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleIdeaChange = (event) => {
+    setIdea(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
     // Handle form submission here
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      {/* Form fields */}
-    </form>
+    <div className="form-container">
+      <h1 className="form-title">Sustainable City Ideas Form</h1>
+      <form className="idea-form" onSubmit={handleSubmit}>
+        <div className="input-group">
+          <label htmlFor="firstName">First Name:</label>
+          <input type="text" id="firstName" name="firstName" required />
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="lastName">Last Name:</label>
+          <input type="text" id="lastName" name="lastName" required />
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="contact">Email Address or Phone Number:</label>
+          <input type="text" id="contact" name="contact" required />
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="submissionType">Submission Type:</label>
+          <select id="submissionType" name="submissionType" onChange={handleSubmissionTypeChange} value={submissionType}>
+            <option value="text">Text</option>
+            <option value="video">Video</option>
+          </select>
+        </div>
+
+        {submissionType === 'video' && (
+          <div className="input-group">
+            <label htmlFor="videoUpload">Upload Video:</label>
+            <input type="file" id="videoUpload" name="videoUpload" accept="video/*" onChange={handleFileChange} />
+          </div>
+        )}
+
+        {submissionType === 'text' && (
+          <div className="input-group">
+            <label htmlFor="ideaText">Describe Your Idea (max 250 words):</label>
+            <textarea id="ideaText" name="ideaText" maxLength="250" value={idea} onChange={handleIdeaChange} />
+          </div>
+        )}
+
+        <div className="input-group">
+          <label htmlFor="areaOfFocus">Area of Focus:</label>
+          <select id="areaOfFocus" name="areaOfFocus">
+            <option value="transportation">Transportation and Energy</option>
+            <option value="greenBuildings">Green Buildings</option>
+            {/* Add more options here */}
+          </select>
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="potentialImpact">Potential Impact:</label>
+          <textarea id="potentialImpact" name="potentialImpact" rows="4" required />
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="termsAndConditions">
+            <input type="checkbox" id="termsAndConditions" name="termsAndConditions" required />
+            I agree to the terms and conditions
+          </label>
+        </div>
+
+        <button type="submit" className="submit-button">
+          Submit Idea
+        </button>
+      </form>
+    </div>
   );
 };
 
-export default FormComponent;
+export default YourFormComponent;
+
