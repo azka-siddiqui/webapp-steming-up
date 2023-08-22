753e763626b24a1e1ad0a3c3a297dca5e38d84e1
Azka Siddiqui <azkasiddiqui240@gmail.com>
2023-08-21 22:43:16 -0400

    changes

--- components/form/form.jsx
+++ components/form/form.jsx
@@ -20,61 +20,23 @@ const Form = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    
+    // Your form submission logic here
   };
 
   return (
     <div className="form-container">
-     <h1 class="form-title">Get started here</h1>
-    <h2 style="font-size: 15px; color: grey;"> Fill out our innovation form</h2>
+      <h1 className="form-title">Get started here</h1>
+      <h2 style={{ fontSize: '15px', color: 'grey' }}>Fill out our innovation form</h2>
       <form className="idea-form" onSubmit={handleSubmit}>
-        <div className="input-group">
-          <label htmlFor="firstName">First Name:</label>
-          <input type="text" id="firstName" name="firstName" required />
-        </div>
-
-        <div className="input-group">
-          <label htmlFor="lastName">Last Name:</label>
-          <input type="text" id="lastName" name="lastName" required />
-        </div>
-
-        <div className="input-group">
-          <label htmlFor="contact">Email Address or Phone Number:</label>
-          <input type="text" id="contact" name="contact" required />
-        </div>
-
-        <div className="input-group">
-          <label htmlFor="submissionType">Submission Type:</label>
-          <select id="submissionType" name="submissionType" onChange={handleSubmissionTypeChange} value={submissionType}>
-            <option value="text">Text</option>
-            <option value="video">Video</option>
-          </select>
-        </div>
-
-        {submissionType === 'video' && (
-          <div className="input-group">
-            <label htmlFor="videoUpload">Upload Video:</label>
-            <input type="file" id="videoUpload" name="videoUpload" accept="video/*" onChange={handleFileChange} />
-          </div>
-        )}
-
-        {submissionType === 'text' && (
-          <div className="input-group">
-            <label htmlFor="ideaText">Describe Your Idea (max 250 words):</label>
-            <textarea id="ideaText" name="ideaText" maxLength="250" value={idea} onChange={handleIdeaChange} />
-          </div>
-        )}
-
+        {/* ...other input fields... */}
+        
         <div className="input-group">
           <label htmlFor="areaOfFocus">Area of Focus:</label>
           <select id="areaOfFocus" name="areaOfFocus">
             <option value="transportation">Transportation and Energy</option>
             <option value="greenBuildings">Green Buildings</option>
-            <option value="waterconservation"> Water Conservation</option>
-            <option value=""> </option>
-            <option value=""> </option>
-            <option value=""> </option>
-   
+            <option value="waterconservation">Water Conservation</option>
+            {/* ...other options... */}
           </select>
         </div>
 
@@ -99,4 +61,3 @@ const Form = () => {
 };
 
 export default Form;
-
