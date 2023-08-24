8c2f7df93caf9395074d9b4ccc59f8dc045dc452
{rnue6} <{rahimaomar8@gmail.com}>
2023-08-23 22:39:27 -0400

    adding text to sponsor page

--- components/SponsorsInfo.jsx
+++ components/SponsorsInfo.jsx
@@ -10,8 +10,8 @@ import {
 export const SponsorsInfo = () => {
     return(
         <>
-            <Box textAlign="center" maxW="75%" mx="auto" marginBottom="5%" bg="#444261" padding="20px">
-                <Heading as="h1" size="3xl" color="white" fontWeight="bold">
+            <Box textAlign="center" maxW="75%" mx="auto" marginBottom="5%" bg="white" padding="20px" mt={30}>
+                <Heading as="h1" size="3xl" color="#32a852" fontWeight="bold">
                     Sponsors
                 </Heading>
 
@@ -31,9 +31,29 @@ export const SponsorsInfo = () => {
                 </SimpleGrid>    
             </Box>
 
-            <Box color="white" maxW="75%">
-                <Text>
-                    INSERT SPONSOR BENEFITS HERE !!!!!!!!!!!!!!!!!!!!!!
+            <Box color="white" maxW="75%" mx="auto">
+                <Heading mb={10} color="#c9ac2c">
+                    Why should you sponsor OurCity?
+                </Heading>
+                
+                <Text ml={20}>
+                    OurCity sponsors receive a variety of benefits for supporting our non-profit organization. To outline a few:
+                        <br/> <br/>
+                </Text>
+                <Text ml={20} color="#32a852">
+                    <ul>
+                        <li>
+                            Increased brand awareness: OurCity features sponsor logos and names across our home site and other media. Your brand will become associatedwith our cause.
+                        </li>
+                        <br/>
+                        <li>
+                            Introduction to a new audience: Through OurCity sponsors can connect to a new audience and pitch products or services to a demographic that might not be familiar with them.
+                        </li>
+                        <br/>
+                        <li>
+
+                        </li>
+                    </ul>
                 </Text>
             </Box>
         </>
