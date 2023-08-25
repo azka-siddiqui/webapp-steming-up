c477fa7ef902fbff745f08f8d6ddb2dd6b3d45e6
{rnue6} <{rahimaomar8@gmail.com}>
2023-08-24 21:35:18 -0400

    re-add api

---
+++ pages/api/subscribe.js
@@ -0,0 +1,28 @@
+// CODE FOR MAIL STORING (UNUSED)
+
+
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+
+export default async function handler(req, res) {
+    const toEmail = req.body.email;
+
+
+    const doc = new GoogleSpreadsheet('11JaWSnUXRrKh0AhMmbBRhJqf4mWHhGMn8r4eyz4eaFo');
+    await doc.useServiceAccountAuth({
+        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY,
+    });
+
+
+    await doc.loadInfo()
+
+    const index = doc.sheetsByIndex.findIndex(sheet => sheet.title === 'Sheet1');
+
+    const sheet = doc.sheetsByIndex[index];
+    const date = new Date().toLocaleDateString("en-US");
+    await sheet.addRow({ Timestamp: date, Email: toEmail });
+
+    res.status(200).json({
+        accepted: true
+    });
+}
