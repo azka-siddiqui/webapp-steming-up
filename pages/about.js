52c355c4eeaf7feb5a1c6f9c0cd325c35956f636
Taehyeon Kim <1234tgk@gmail.com>
2023-08-25 00:10:17 -0400

    fix header issue within about page

--- pages/about.js
+++ pages/about.js
@@ -2,59 +2,73 @@ import Head from "next/head";
 import { NavBar } from "../components/NavBar";
 import { Footer } from "../components/Footer";
 
-import {
-    Box,
-    Heading,
-    Text
-  } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
-  export default function about() {
-    return (
+export default function about() {
+  return (
+    <Box bgPos='center' bgRepeat='repeat' minH='100vh' bg='rgb(21, 20, 38)'>
+      <Box
+        display='flex'
+        flexDirection='column'
+        justifyContent='center'
+        alignItems='center'
+      >
+        <Head>
+          <title>OurCity - About</title>
+          <link rel='logo' href='/Logo.png' />
+        </Head>
+
+        <NavBar bg='#151426' />
+
         <Box
-        bgPos="center"
-        bgRepeat="repeat"
-        minH="100vh" // Adjust min height for full page coverage
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-        bg="rgb(21, 20, 38)">
-            <Head>
-            <title>OurCity - About</title>
-            <link rel="logo" href="/Logo.png" />
-            </Head>
+          textAlign='center'
+          maxW='1000px'
+          id='about'
+          mt='30px'
+          bg='#444261'
+          padding='30px'
+          margin='30px'
+        >
+          <Heading
+            as='h1'
+            size='3xl'
+            color='white'
+            fontWeight='bold'
+            textAlign='center'
+          >
+            About{" "}
+            <Text as='span' color='#32a852'>
+              OurCity
+            </Text>
+          </Heading>
 
-            <NavBar bg="#151426"/>
+          <Box fontSize='xl' color='white' mt='40px'>
+            <Text>
+              From our conversations with urban planners working in both
+              government and consulting, we know that many planners recognize
+              the importance of engaging youth but don’t know how to effectively
+              incorporate it into their work. Similarly, youth do not know how
+              they can get involved in their cities urban planning initiatives.
+              OurCity is here to change that - youth can share their ideas for
+              changes to their local community or city to make the
+              infrastructure more sustainable.
+            </Text>
 
-            <Box textAlign = "center" maxW="1000px" id="about" mt = "30px" bg="#444261" padding="30px" margin="30px">
-                
-                <Heading as="h1" size="3xl" color = "white" fontWeight="bold" textAlign="center">
-                About{" "}
-                <Text as="span" color="#32a852">
-                    OurCity
-                </Text>
-                </Heading>
-
-                <Box fontSize="xl" color="white" mt="40px">
-                    <Text>
-                    From our conversations with urban planners working in both government and consulting, we 
-                    know that many planners recognize the importance of engaging youth but don’t know how to effectively 
-                    incorporate it into their work. Similarly, youth do not know how they can get involved in their cities urban planning initiatives. OurCity is here to change that - youth can share their ideas for changes to their 
-                    local community or city to make the infrastructure more sustainable. 
-
-                    </Text>
-
-                    <Text mt="8">
-                    Youth can fill out our form about a project or idea that they would like to see implemented in their community. We will then work with our partners to help bring this vision to life alongside young people. 
-                    From outreach to ideation to feedback and follow-up, our program is a tangible way for youth to kickstart their journey in urban planning and make an impact in their community. We also host workshops as a helpful resource for those looking to better 
-                    create livable communities — cities for everyone.
-                    </Text>
-                        
-                </Box>
-
-            </Box>
-        
-            <Footer />
+            <Text mt='8'>
+              Youth can fill out our form about a project or idea that they
+              would like to see implemented in their community. We will then
+              work with our partners to help bring this vision to life alongside
+              young people. From outreach to ideation to feedback and follow-up,
+              our program is a tangible way for youth to kickstart their journey
+              in urban planning and make an impact in their community. We also
+              host workshops as a helpful resource for those looking to better
+              create livable communities — cities for everyone.
+            </Text>
+          </Box>
         </Box>
-    )
+
+        <Footer />
+      </Box>
+    </Box>
+  );
 }
